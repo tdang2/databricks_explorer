4053b767280e467f5eb7e7848c12386bc5dc4419
tdang2 <quangtrister@gmail.com>
2018-06-08 19:38:04 -0400

    second commit

--- notebooks/Users/tri.dang@innitico.com/managed_table.py
+++ notebooks/Users/tri.dang@innitico.com/managed_table.py
@@ -1,1 +1,39 @@
 # Databricks notebook source
+import pandas as pd
+import numpy as np
+spark.conf.set("spark.sql.execution.arrow.enabled", "true")
+
+# COMMAND ----------
+
+spark.conf.set("dfs.adls.oauth2.access.token.provider.type", "ClientCredential")
+spark.conf.set("dfs.adls.oauth2.client.id", "1d76bb0b-c509-49ff-8639-23c5e1bb9b4d")
+spark.conf.set("dfs.adls.oauth2.credential", "K/k2d/Ane1/mvghYsY1tbE0/6xTdP/92uQafJMYTXnQ=")
+spark.conf.set("dfs.adls.oauth2.refresh.url", "https://login.microsoftonline.com/c622fc2d-f138-46eb-a729-3aa0cce44410/oauth2/token")    
+
+# COMMAND ----------
+
+apsupp_df = sqlContext.read.parquet("adl://staplespocnoencryption.azuredatalakestore.net/poc_table/apsupp")
+coystr_df = sqlContext.read.parquet("adl://staplespocnoencryption.azuredatalakestore.net/poc_table/coystr")
+invmst_df = sqlContext.read.parquet("adl://staplespocnoencryption.azuredatalakestore.net/poc_table/invmst")
+inybal_df = sqlContext.read.parquet("adl://staplespocnoencryption.azuredatalakestore.net/poc_table/inybal")
+rplstdet_df = sqlContext.read.parquet("adl://staplespocnoencryption.azuredatalakestore.net/poc_table/rplstdet")
+display_df = sqlContext.read.parquet("adl://staplespocnoencryption.azuredatalakestore.net/poc_table/display")
+critical_df = sqlContext.read.parquet("adl://staplespocnoencryption.azuredatalakestore.net/poc_table/critical")
+out_lvl_df = sqlContext.read.parquet("adl://staplespocnoencryption.azuredatalakestore.net/poc_table/out_lvl")
+inystr_df = sqlContext.read.parquet("adl://staplespocnoencryption.azuredatalakestore.net/poc_table/inystr")
+
+# COMMAND ----------
+
+# Saving as databricks managed table: databricks stores both data and schema
+apsupp_df.write.mode('overwrite').format("parquet").saveAsTable('apsupp')
+coystr_df.write.mode('overwrite').format("parquet").saveAsTable('coystr')
+invmst_df.write.mode('overwrite').format("parquet").saveAsTable('invmst')
+inybal_df.write.mode('overwrite').format("parquet").saveAsTable('inybal')
+inystr_df.write.mode('overwrite').format("parquet").saveAsTable('inystr')
+rplstdet_df.write.mode('overwrite').format("parquet").saveAsTable('rplstdet')
+display_df.write.mode('overwrite').format("parquet").saveAsTable('display')
+critical_df.write.mode('overwrite').format("parquet").saveAsTable('critical')
+out_lvl_df.write.mode('overwrite').format("parquet").saveAsTable('out_lvl')
+
+# COMMAND ----------
+
